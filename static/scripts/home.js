e8ab59cc2c8a6aad8973d9d378b5fccff5723505
walid <walid.smali.mastari@gmail.com>
2023-08-26 15:41:39 +0100

    prog

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -4,9 +4,30 @@ for(let i=0; i < updatebuttons.length; i++){
     let bookISBN = this.dataset.book
     let action = this.dataset.action
     console.log(bookISBN, action)
-    console.log(user)
     if(user === 'AnonymousUser'){
       console.log('anon user')
-    }else{console.log('sending data')}
+    }else{
+      console.log('user is logged');
+      updateuserorder(bookISBN, action);
+    }
   })
+}
+
+function updateuserorder(bookISBN, action){
+  let url = '/pages/updateitem/'
+  fetch(url, {
+    method: 'POST',
+    headers:{
+      'Content-Type' : 'application/json',
+      'X-CSRFTOKEN': csrftoken,
+    },
+    body:JSON.stringify({'bookISBN': bookISBN, 'action': action})
+  })
+  .then((Response) =>{
+    return Response.json()
+  })
+  .then((data) =>{
+    console.log(data)
+  })
+
 }
