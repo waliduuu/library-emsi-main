fcab6a3171311ba9d6eee5c91d4c1da123679bd0
walid <walid.smali.mastari@gmail.com>
2023-08-27 19:20:01 +0100

    updatded all pages

--- library/urls.py
+++ library/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('home/',views.home, name='home'),
     path('myshelf/',views.shelf, name='myshelf'),
-    path('updateitem/',views.updateitem, name='updateitem'),
+    path('confirmation/',views.confirmation, name='confirmation'),
 ]
